c4f4df01b5f12e99363052e8c6e182a84b3b23a0
garie <garie@SG02.local>
2025-11-30 10:22:23 +0800

    fix vite alias for GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5176',
-        changeOrigin: true
-      }
-    }
-  }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
